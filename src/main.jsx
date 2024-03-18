81ba6c75d47dde7f13650c4c6e8cb8581feea314
Murillo Alves Couto <murilloacouto@outlook.com>
2024-03-18 12:56:03 -0300

    refatoramento dos códigos previamente fornecidos no desafio

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.scss';
-import Home from './views/Home/Home';
-import Livros from './views/Livros/Livros';
-import LivrosCadastro from './views/LivrosCadastro/LivrosCadastro';
-import LivrosEdicao from './views/LivrosEdicao/LivrosEdicao';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.scss";
+import Home from "./views/Home/Home";
+import Livros from "./views/Livros/Livros";
+import LivrosCadastro from "./views/LivrosCadastro/LivrosCadastro";
+import LivrosEdicao from "./views/LivrosEdicao/LivrosEdicao";
 
 const router = createBrowserRouter([
   {
@@ -17,7 +14,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/livros",
-    element: <Livros/>,
+    element: <Livros />,
   },
   {
     path: "/livros/cadastro",
@@ -29,8 +26,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
