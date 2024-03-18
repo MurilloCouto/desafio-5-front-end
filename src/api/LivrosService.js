c51209e4971c0e2a2a9353d991f39b14d49ade21
Murillo Alves Couto <murilloacouto@outlook.com>
2024-03-18 12:29:32 -0300

    alterando url base na api

--- src/api/LivrosService.js
+++ src/api/LivrosService.js
@@ -1,26 +1,25 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://desafio-5-back-end.vercel.app/";
 
-export class LivrosService{
-    static getLivros(){
-        return axios.get(BASE_URL+'/livros');
-    }
+export class LivrosService {
+  static getLivros() {
+    return axios.get(BASE_URL + "/livros");
+  }
 
-    static getLivro(id){
-        return axios.get(`${BASE_URL}/livros/edicao/${id}`);
-    }
+  static getLivro(id) {
+    return axios.get(`${BASE_URL}/livros/edicao/${id}`);
+  }
 
-    static createLivro(body){
-        return axios.post(`${BASE_URL}/livros/cadastro`,body);
-    }
+  static createLivro(body) {
+    return axios.post(`${BASE_URL}/livros/cadastro`, body);
+  }
 
-    static updateLivro(id,body){
-        return axios.put(`${BASE_URL}/livros/edicao/${id}`,body);
-    }
+  static updateLivro(id, body) {
+    return axios.put(`${BASE_URL}/livros/edicao/${id}`, body);
+  }
 
-    static deleteLivro(id){
-        return axios.delete(`${BASE_URL}/livros/${id}`);
-    }
-    
+  static deleteLivro(id) {
+    return axios.delete(`${BASE_URL}/livros/${id}`);
+  }
 }
